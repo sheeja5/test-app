b9d9ed7552762f76d2e10e6eaa4ade1265afaa0d
sheeja5 <sheeja.stc@gmail.com>
2024-12-17 19:58:21 +0530

    Update Abel.py

--- Abel.py
+++ Abel.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 st.title ("hello")
 apikey="AIzaSyDNcp_JDchji_PfU6mKwjCz2SxGBj0a4Ow"
-question = st.text_input()
+question = st.text_input("hello")
 genai.configure(api_key="apikey")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content(question)
